60f3e4c92e999f0f103f60a5d5ca1ffa3c6d79e9
Guilherme Lazari <guilhermelcs@gmail.com>
2023-04-22 20:57:47 +0100

    Add clap to choose between repl and server mode

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,39 @@ mod parser;
 mod repl;
 mod server;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about= None)]
+struct Args {
+    #[clap(subcommand)]
+    mode: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Server { port: u16 },
+    Repl,
+}
+
+
+
 fn main() {
+    let args = Args::parse();
+    match args.mode {
+        Commands::Server { port } => {
+            let options = server::ServerOptions::new(port);
+            server::server(options);
+        }
+        Commands::Repl => {
+            let datastore = datastore::DataStore::new();
+            repl::repl(datastore);
+        }
+    }
+    
+}
+
+fn self_standing_server() {
     let options = server::ServerOptions::new(8080);
     server::server(options);
 }
